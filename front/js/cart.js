60f25ed7fb9eb19975b12154de36367e0a66c7f1
Matisse Goby <genesissssss@gmail.com>
2021-12-23 21:00:57 +0100

    Check email

--- front/js/cart.js
+++ front/js/cart.js
@@ -164,24 +164,49 @@ function userInputVerification() {
     }
   }
 
- function adressValid() {
-		const userAdress = userForm.adress;
-		const addressErrorMsg = document.getElementById("addressErrorMsg");
-		if (/[^§]{5,50}$/.test(userAdress)) {
-			addressErrorMsg.innerText = "";
-			return true;
-		} else {
-			addressErrorMsg.innerText = "L'adresse semble incorrect.";
-		}
-	}
-  
+  function adressValid() {
+    const userAdress = userForm.adress;
+    const addressErrorMsg = document.getElementById("addressErrorMsg");
+    if (/[^§]{5,50}$/.test(userAdress)) {
+      addressErrorMsg.innerText = "";
+      return true;
+    } else {
+      addressErrorMsg.innerText = "L'adresse semble incorrect.";
+    }
+  }
+
   function cityValid() {
-		const userCity = userForm.city;
-		const cityErrorMsg = document.getElementById("cityErrorMsg");
-		if (/^[A-Za-z]{2,20}$/.test(userCity)) {
-			cityErrorMsg.innerText = "";
-			return true;
-		} else {
-			cityErrorMsg.innerText = "La ville ne peut contenir que des lettres, de 2 à 20 caractères.";
-		}
-	}
+    const userCity = userForm.city;
+    const cityErrorMsg = document.getElementById("cityErrorMsg");
+    if (/^[A-Za-z]{2,20}$/.test(userCity)) {
+      cityErrorMsg.innerText = "";
+      return true;
+    } else {
+      cityErrorMsg.innerText =
+        "La ville ne peut contenir que des lettres, de 2 à 20 caractères.";
+    }
+  }
+
+  function emailValid() {
+    const userEmail = userForm.email;
+    const emailErrorMsg = document.getElementById("emailErrorMsg");
+    if (/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(userEmail)) {
+      emailErrorMsg.innerText = "";
+      return true;
+    } else {
+      emailErrorMsg.innerText = "Il faut renseigner une adresse email valide.";
+    }
+  }
+
+  if (
+    firstNameValid() &&
+    lastNameValid() &&
+    adressValid() &&
+    cityValid() &&
+    emailValid()
+  ) {
+    return true;
+  } else {
+    console.log("Unvalid form input.");
+  }
+}
