55c94c32776c42ad6ccd7fb52e8f2a304b16b91b
Matisse Goby <genesissssss@gmail.com>
2021-12-24 22:05:47 +0100

    Getting the order Id with the URL

---
+++ front/js/confirmation.js
@@ -0,0 +1,14 @@
+function idRecuperation() {
+  let url = new URL(window.location.href);
+  let searchParams = new URLSearchParams(url.search);
+  if (searchParams.has("id")) {
+    let id = searchParams.get("id");
+    return id;
+  } else {
+    console.log("Error, no order Id found");
+  }
+}
+window.addEventListener("load", () => {
+  const orderId = document.getElementById("orderId");
+  orderId.innerText = idRecuperation();
+});
