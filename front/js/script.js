1200db744a6791e093b8545c1da8bbf6d9645507
Matisse Goby <genesissssss@gmail.com>
2021-12-27 19:49:27 +0100

    modify comment

--- front/js/script.js
+++ front/js/script.js
@@ -1,4 +1,4 @@
-// Get all the information from Bilal's API
+// Get all the information from the API
 async function getItems() {
   try {
     let response = await fetch("http://localhost:3000/api/products");
