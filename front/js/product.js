0bf37706feee59fdcda8b65e24c5402bd042cb51
Matisse Goby <genesissssss@gmail.com>
2021-12-27 16:01:54 +0100

    modify function

--- front/js/product.js
+++ front/js/product.js
@@ -1,26 +1,29 @@
 function idVerification() {
-  let url = new URL(window.location.href);
-  let searchParams = new URLSearchParams(url.search);
-  if (searchParams.has("id")) {
-    let id = searchParams.get("id");
-    return id;
-  } else {
-    console.log("Error, no Id found in the URL");
+  let searchParams = new URLSearchParams(new URL(window.location.href).search);
+
+  if (!searchParams.has("id")) {
+    console.error("Error, no Id found in the URL!");
+    return;
   }
+
+  return searchParams.get("id");
 }
 
 async function getInfoById() {
   let id = idVerification();
   try {
-    let response = await fetch(`http://localhost:3000/api/products/${id}`);
+    let response = await fetch(
+      `http://localhost:3000/api/products/${idVerification()}`
+    );
     return await response.json();
   } catch (error) {
-    console.log("Error : " + error);
+    console.error("Error : " + error);
   }
 }
 
 (async function renderItem() {
-  let item = await getInfoById();
+  const item = await getInfoById();
+
   document.querySelector(
     ".item__img"
   ).innerHTML += `<img src="${item.imageUrl}" alt="${item.altTxt}">`;
@@ -36,17 +39,16 @@ async function getInfoById() {
 
 const addToCartBtn = document.getElementById("addToCart");
 addToCartBtn.addEventListener("click", () => {
-  const itemId = idVerification();
-  const itemColor = document.getElementById("colors").value;
-  const itemQuantity = document.getElementById("quantity").value;
-
-  if (itemColor === "") {
+  if (document.getElementById("colors").value === "") {
     alert("Il est nécessaire de choisir une couleur");
-  } else if (itemQuantity == 0) {
-    alert("Il faut au moins ajouter un Kanap");
-  } else {
-    const itemInCart = [itemId, itemColor];
-    localStorage.setItem(itemInCart, itemQuantity);
-    window.location.href = "./cart.html";
+    return;
   }
+
+  if (document.getElementById("quantity") == 0) {
+    alert("Il faut au moins ajouter un Kanap");
+    return;
+  }
+
+  localStorage.setItem([itemId, itemColor], itemQuantity);
+  window.location.href = "./cart.html";
 });
