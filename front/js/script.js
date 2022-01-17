931ec7af85dfb2015cde0b6ae7e809a79fe43b8a
Matisse Goby <genesissssss@gmail.com>
2022-01-17 15:26:38 +0100

    using createElement to do the render on the HTML

--- front/js/script.js
+++ front/js/script.js
@@ -1,29 +1,50 @@
 // Get all the information from the API
 async function getItems() {
   try {
-    let response = await fetch("http://localhost:3000/api/products");
+    let response = await fetch('http://localhost:3000/api/products');
     return await response.json();
   } catch (error) {
-    console.log("Error : " + error);
+    console.error('Error : ' + error);
   }
 }
 
 // Handle the render on the HTML
 (async function renderItems() {
-  let items = await getItems();
-  let htmlRender = "";
+  const createElementWithClassAndContent = (tag, className, content) => {
+    const element = document.createElement(tag);
+    element.classList.add(className);
+    element.appendChild(document.createTextNode(content));
+
+    return element;
+  };
+
+  const items = await getItems();
+  const itemsContainer = document.getElementById('items');
+
   items.forEach((item) => {
-    let htmlContent = `
-		<a href="./product.html?id=${item._id}">
-			<article>
-				<img src="${item.imageUrl}" alt="${item.altTxt}">
-				<h3 class="productName">${item.name}</h3>
-				<p class="productDescription">${item.description}</p>
-			</article>
-		</a>
-		`;
-    htmlRender += htmlContent;
+    const itemArticle = document.createElement('article');
+
+    const itemImg = document.createElement('img');
+    itemImg.setAttribute('src', item.imageUrl);
+    itemImg.setAttribute('alt', item.altTxt);
+    itemArticle.appendChild(itemImg);
+
+    itemArticle.appendChild(
+      createElementWithClassAndContent('h3', 'productName', item.name)
+    );
+
+    itemArticle.appendChild(
+      createElementWithClassAndContent(
+        'p',
+        'productDescription',
+        item.description
+      )
+    );
+
+    const itemContainer = document.createElement('a');
+    itemContainer.href = `./product.html?id=${item._id}`;
+    itemContainer.appendChild(itemArticle);
+
+    itemsContainer.appendChild(itemContainer);
   });
-  const itemContainer = document.getElementById("items");
-  itemContainer.innerHTML += htmlRender;
 })();
